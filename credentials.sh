1635da53b22167bb88ad22203d171198997945e2
Connor Rowland <connor.rowland@zymbit.com>
2021-02-18 16:33:15 -0800

    Fixed merge

--- credentials.sh
+++ credentials.sh
@@ -3,7 +3,6 @@
 ###############################
 # Start configuration section #
 ###############################
-<<<<<<< HEAD
 CERT=/opt/zymbit/device.crt
 CACERT=/opt/zymbit/root.ca.pem
 
@@ -16,27 +15,11 @@ KEY="pkcs11:object=iotkey;type=private;token=zymbit;pin-value=1234"
 
 # This can be obtained by running - aws iot describe-endpoint --endpoint-type iot:CredentialProvider --output text
 CREDENTIAL_PROVIDER_URL="$CREDENTIAL_URL.credentials.iot.$REGION.amazonaws.com"
-=======
-REGION=us-east-2
-THING_NAME=Test1
-ROLE_ALIAS_NAME=deviceRoleAlias
-KEY="pkcs11:object=iotkey;type=private;token=zymbit;pin-value=1234"
-
-# This can be obtained by running - aws iot describe-endpoint --endpoint-type iot:CredentialProvider --output text
-# NOTE: Do not include a trailing slash or any https:// prefix
-CREDENTIAL_PROVIDER_URL="c1vvdcbkn5au83.credentials.iot.us-east-2.amazonaws.com"
->>>>>>> 6c573427e95940168ea5f59d625580302b0dfdb9
 ###############################
 # End configuration section   #
 ###############################
 
-<<<<<<< HEAD
 FULL_URL="https://"$CREDENTIAL_PROVIDER_URL"/role-aliases/"$ROLE_ALIAS"/credentials"
-=======
-CERT=/opt/zymbit/iot.crt
-CACERT=/opt/zymbit/root.ca.pem
-FULL_URL="https://"$CREDENTIAL_PROVIDER_URL"/role-aliases/"$ROLE_ALIAS_NAME"/credentials"
->>>>>>> 6c573427e95940168ea5f59d625580302b0dfdb9
 PKCS11_ENGINE_FOR_CURL="--engine zymkey_ssl --key-type ENG"
 
 if [ ! -f "$CERT" ]; then
@@ -50,11 +33,7 @@ if [ ! -f "$CACERT" ]; then
 fi
 
 # No longer using the AWS IoT Verisign root CA.  If the distro doesn't have certificate authorities installed this command will probably fail
-<<<<<<< HEAD
 CREDENTIALS=$(curl --cacert $CACERT --cert $CERT $PKCS11_ENGINE_FOR_CURL --key $KEY -H "x-amzn-iot-thingname: $THING_NAME" $FULL_URL >& /dev/null)
-=======
-CREDENTIALS=$(curl --cacert $CACERT --cert $CERT $PKCS11_ENGINE_FOR_CURL --key $KEY -H "x-amzn-iot-thingname: $THING_NAME" $FULL_URL)
->>>>>>> 6c573427e95940168ea5f59d625580302b0dfdb9
 
 returnValue=$?
 
@@ -72,8 +51,4 @@ export AWS_SESSION_TOKEN=$(jq --raw-output .credentials.sessionToken <(echo $CRE
 
 export EXPIRATION=$(date --date="+120 seconds" -Iseconds)
 
-<<<<<<< HEAD
 #echo "{ \"Version\": 1, \"AccessKeyId\": \"$AWS_ACCESS_KEY_ID\", \"SecretAccessKey\": \"$AWS_SECRET_ACCESS_KEY\", \"SessionToken\": \"$AWS_SESSION_TOKEN\", \"Expiration\": \"$EXPIRATION\" }"
-=======
-echo "{ \"Version\": 1, \"AccessKeyId\": \"$AWS_ACCESS_KEY_ID\", \"SecretAccessKey\": \"$AWS_SECRET_ACCESS_KEY\", \"SessionToken\": \"$AWS_SESSION_TOKEN\", \"Expiration\": \"$EXPIRATION\" }"
->>>>>>> 6c573427e95940168ea5f59d625580302b0dfdb9
